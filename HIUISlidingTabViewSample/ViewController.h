1409c750203153449b336a76772376cd18fb5413
Sunhong Kim <shkim@sunhong-ui-imac.local>
2016-06-22 23:55:20 +0900

    Initial Commit

---
+++ HIUISlidingTabViewSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HIUISlidingTabViewSample
+//
+//  Created by Sunhong Kim on 2016. 6. 22..
+//  Copyright © 2016년 Sunhong Kim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HIUISlidingTabViewSample/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  HIUISlidingTabViewSample
+//
+//  Created by Sunhong Kim on 2016. 6. 22..
+//  Copyright © 2016년 Sunhong Kim. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
